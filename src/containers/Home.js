86c87e446994f1b76a7cc652e7092e412da1a8e6
kireevroi <kireevroi@gmail.com>
2023-05-03 16:28:38 +0400

    first commit

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Kanban GO</h1>
+        <p className="text-muted">Open Source Kanban Board</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/containers/Login.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./Login.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validateForm = () => {
+    return email.length > 0 && password.length > 0;
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const credentials = {
+      user: email,
+      password: password,
+    };
+    axios.post('http://kanbango.ru:5535/api/login', credentials)
+    .then(response => {
+      console.log(response);
+    })
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block="true" size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default Login;
